654aaa8a27974bfb58ddb92470ad8a33f7d42e04
marcoroca <mroca@mc4.com.bo>
2023-06-02 11:08:25 -0400

    nuevo header

--- web/cafeteria.php
+++ web/cafeteria.php
@@ -6,48 +6,14 @@ include("include/dbopen.php");
 //PAGINA
 include("include/head.php");
 include("include/TablasDinamicas.php");
-include("menus/menuCafeteria");
-//variables para iniciar sesion
-	session_start();
-	$user = $_SESSION['funcionario'];
-	$tipo = $_SESSION['su'];
 
+ //variables para iniciar sesion
+        session_start();
+        $user = $_SESSION['funcionario"'];
+        $tipo = $_SESSION['su'];
 //variables del FORMS
 ?>
 
-      <div class="container">
-        <div class="row">
-          <div class="col" style="margin-top: 15px">
-            <?php
-            	//TOP
-      		echo "<h1 align='left' style=\"margin-left: 30px\"> Cafeteria SCIS </h1>";
-      		echo "<a align='left'style=\"margin-left: 45px; font-size: 24px;\"> <b> Usuario: </b> <u> $user </u> </a>  <br>";
-      		echo "<a style=\"margin-left: 45px; font-size: 24px\"> <b> Fecha : </b> obtenerFecha </a>  <br><br>";
-      		echo "<h2 style=\"margin-left: 20px\"> Buscador: </h2>";
-            ?>
-
-          </div>
-          <div class="col" align="center" style="margin-top: 28px;">
-            <!--   Botones de interaccion cafeteria   -->
-           <form>
-            
-            <input type="submit" name="cerrarS" value="Cerrar Sesion" style="width: 360px; background-color: #BBBBBB; border-radius: 12px;">
-          </BR>
-            
-            <input type="submit" name="Añadir_Padre" value="Añadir Padre" style="margin-top: 10px; background-color: #BBBBBB; border-radius: 12px;" formaction="creacionUsuarios/creacionPadre.php">
-           
-      
-            <input type="submit" name="Añadir_Docente" value="Añadir Docente" style="margin-top: 10px; background-color: #BBBBBB; border-radius: 12px;" formaction="creacionUsuarios/creacionDocente.php">
-      
-            
-            <input type="submit" name="Añadir_Estudiante" value="Añadir Estudiante" style="margin-top: 10px; background-color: #BBBBBB; border-radius: 12px;" formaction="creacionUsuarios/creacionEstudiante.php">
-            <BR>
-            <input type="submit" name="Scanner" value="Scanner" style="margin-top: 10px; width: 360; background-color: #BBBBBB; border-radius: 12px;" formaction="qr_e.php">
-        	  </BR>
-            <input type="button" name="Lista_Actualizacion_Continua" value="Lista Actualizacion Continua" style="margin-top: 10px; width: 360px; background-color: #BBBBBB; border-radius: 12px;">
-           </form>
-          </div>
-        </div>
   <!-- Bandeja Lista Estudiantes Continua -->
 	 <?php
 	 //query que debe tar en otro .php
@@ -67,7 +33,6 @@ include("menus/menuCafeteria");
             });
       });
 </script>
-</div>
 
 
 </body>
--- web/include/head.php
+++ web/include/head.php
@@ -12,5 +12,81 @@
   <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"></script>
+
+
+
+
+
+
+<style type="text/css">
+  .fondo_azul {
+    background: #071655;
+    color: #FFFFFF;
+    display:  flex;
+    padding-top: 30px;
+  }
+  h1 {
+    font-size: 40px;
+    display:  flex;
+  }
+  .boton input{
+    background: #071655;
+    font-size: 16px;
+    color: #FFFFFF;
+    border-color: yellow;
+    border-bottom: none;
+    text-align: center;
+    border-radius: 10px 10px 0 0;
+    width: 100%;
+    height: 50px;
+    white-space: normal;
+    font-size: 15px;
+  }
+  .boton input:hover {
+    background: #ffffff;
+    color: #071655;
+  }
+
+  @media only screen and (max-width: 809px) {
+      .boton input{
+        font-size: 13px;
+      }
+  }
+  @media only screen and (max-width: 712px) {
+      .boton input{
+        font-size: 10px;
+      }
+  }
+</style>
+<div class="fondo_azul" style>
+  <div style="width: 70%; display:  flex;">
+    <img src="menus/logo_scis" style="width: 50px; height: 50px">
+    <h1>CAFETERIA SCIS</h1>
+  </div>
+  <div style="width: 30%; text-align: right; padding-right: 30px;">
+    <div>
+      <?php
+     
+      ?>
+      Usuario: <?php echo "NOMBRE$usuario";  ?>
+    </div>
+    <div>
+      Fecha: 
+    </div>
+  </div>
+</div>
+<div class="boton fondo_azul">
+  <div style="width: 20%;">
+    <input class="boton" type="submit" name="Buscador" value="Buscador" formaction="creacionUsuarios/creacionDocente.php">
+  </div>
+  <div style="width: 20%;">
+    <input class="boton" type="submit" name="Lista de Actualizacion Continua" value="Lista de Actualizacion Continua" formaction="creacionUsuarios/creacionDocente.php">
+  </div>
+  <div style="width: 20%;">
+    <input class="boton" type="submit" name="Escanear" value="Escanear" formaction="creacionUsuarios/creacionDocente.php">
+  </div>
+</div>
+
 </head>
 <body>
+
